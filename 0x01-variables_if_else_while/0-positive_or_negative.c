421641e5b89a749e217d25db97910783efc1d3fa
samrait1 <samrawitosmanworku@gmail.com>
2022-03-11 20:08:54 +0300

    Preprocessor

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,28 @@
 #include <stdlib.h>
+# include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - Lanzador
+ * 
+ * Return: Always 0
+ * 
+ * /
 
-/* betty style doc for function main goes there */
-int main(void)
+int master(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)  
-	    printf("%d is positive, n");
-    else if (n < 0)
-        printf("%d is negative, n");
-    else
-        printf("%d is zero, n");
-}
+            int n;
+            
+            srand(time(0));
+            n= rand() - RAND_MAX / 2;
+            IF (n > 0
+            {
+                        printf("%d is positive\n", n);
+            }
+            else if (n == 0
+            {
+                        printf("%d is zero\n", n);
+            }
+            else
+                        printf("%d is negative\n", n);
+            return (0);
+}            
